32bb555880d2bd8634e54c87a2593809ee55cc05
skanda-2003 <skandazsuresh@gmail.com>
2025-09-22 13:25:26 +0530

    small mistake in v1

--- src/store/sagas/gameFlowSaga.ts
+++ src/store/sagas/gameFlowSaga.ts
@@ -251,7 +251,7 @@ export default function* gameFlowSaga() {
   yield takeEvery(completeGame.type, function* (): Generator<any, void, any> {
     // NEW: Check for whitewash before proceeding to summary
     const gameProgress = yield select(selectGameProgress);
-    const isWhitewash = gameProgress.scores.team1 > 150; // MAX_BID = 250
+    const isWhitewash = gameProgress.scores.team1 === 250; // MAX_BID = 250
     
     if (isWhitewash) {
       console.log("Game Flow Saga: Whitewash detected! Showing celebration animation");
